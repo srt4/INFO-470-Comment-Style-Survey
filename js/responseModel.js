0d42074859da1b4b875497f5f370344f88b10762
Spencer Thomas <srt4@uw.edu>
2011-11-20 17:33:26 -0800

    Added AJAX requests. Streamlined survey response models / tags.

---
+++ js/responseModel.js
@@ -0,0 +1,71 @@
+//$(document).ready(function() {
+	
+	// Initialize the namespace
+	var Survey = {};
+	Survey.responses = {};
+	Survey.sensors = {};
+	/**
+	 * initializeModel ()
+	 * Populates the survey model with default (-1) values. 
+	 */
+	function initializeModel()
+	{
+		$('question').each(function(key, elem){
+			console.log(elem);
+			if ($(elem).hasClass('one'))
+			{
+				console.log('got here');
+				Survey.responses[$(elem).attr('id')] = -1;
+			}
+			
+			if ($(elem).hasClass('many'))
+			{
+				var responses = new Array();
+				$(elem).find('answer').each(function(key, value){
+					responses[$(value).index()] = -1;
+				});
+				Survey.responses[$(elem).attr('id')] = responses;
+			}
+		});
+	}
+	
+	/**
+	 * updateModel(elem)
+	 * @elem AnswersElement 
+	 **/
+	function updateModel (elem) 
+	{
+		// Cases: selectOne, selectMany, freeResponse
+		if ($(elem).hasClass('one'))
+		{
+			Survey.responses[$(elem).attr('id')] = $(elem).find('.selected').index();
+		}
+		
+		// 
+		if ($(elem).hasClass('many'))
+		{
+			$.each($(elem).find('answer'), function(key, value) {
+				Survey.responses[$(elem).attr('id')][$(value).index()] = $(value).hasClass('selected') ? 1 : 0;
+			});
+		}
+		
+		pushAnswers ();
+	}
+	
+	function pushAnswers ()
+	{
+		$.post (
+			'http://www.konspence.com/470research/surveyListener.php',
+			Survey,
+			function(response)
+			{
+				console.log(response);
+			}	
+		);
+	}
+	
+	function paginate ()
+	{
+		
+	}
+//});
--- js/script.js
+++ js/script.js
@@ -1,5 +1,8 @@
 // 
 $(document).ready(function(){
+	// necessary for DB syncing
+	initializeModel();
+
 	$('a').click(function() {
 		if ($(this).attr('data-action') == 'nextPage')
 		{
@@ -17,12 +20,12 @@ $(document).ready(function(){
 	}
 	
 	// Select many
-	$('div selectMany').click(function(){
+	$('div .many answer').click(function(){
 		$(this).toggleClass('selected');
 	});
 	
 	// Select one
-	$('div selectOne').click(function(){
+	$('div .one answer').click(function(){
 		$(this).addClass('selected');
 		$.each($(this).siblings(), function(k,v) {
 			$(v).removeClass('selected');
@@ -30,9 +33,15 @@ $(document).ready(function(){
 	});
 
 	// Select sum
-	$('div selectSum').hover(function(){
+	$('div .scale answer').hover(function(){
 		$(this).prevAll().attr("class", "highlight");
 		$(this).nextAll().attr("class", "");
 		$(this).attr("class", "active");
+	});
+	
+	// Update the model each time a question is updated
+	$('#page2 questions question').click(function(){
+		console.log("Updating model?");
+		updateModel(this);
 	});
 });
